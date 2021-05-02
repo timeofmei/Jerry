e78145785258494f33a07b32af0a1c110458b431
timeofmei <timeofmei@outlook.com>
2021-05-02 14:52:14 +0800

    Initial Commit

---
+++ Jerry/JerryApp.swift
@@ -0,0 +1,17 @@
+//
+//  JerryApp.swift
+//  Jerry
+//
+//  Created by 杨焱焜 on 2021/5/2.
+//
+
+import SwiftUI
+
+@main
+struct JerryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
